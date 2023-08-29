2b15e237782420544a5189ce7df6af736f8506e2
Max Schneider <maxschneidercodes@gmail.com>
2023-08-29 09:59:52 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -14,7 +14,10 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://safetube.eu" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.safetube.eu",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
