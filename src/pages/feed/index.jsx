01eb13e2ef5f4e9a788eca59461f8cbb223c462b
Max Schneider <maxschneidercodes@gmail.com>
2023-08-30 11:35:52 +0200

    fix

--- src/pages/feed/index.jsx
+++ src/pages/feed/index.jsx
@@ -9,13 +9,23 @@ import getFetchURL from "../../../lib/fetchURL"
 import Header from "@/components/ui/header";
 import Spinner from "@/components/spinner";
 import Donate from "@/components/donate";
+import { useEffect } from "react";
+import { checkIfFunctionalityCookiesAreAccepted } from "../../../lib/cookieconsent"
+import { useState } from "react";
 
 export default function DashboardPage() {
   const fetcher = (url) => fetch(url).then((res) => res.json());
 
   const { data, error, isLoading } = useSWR(getFetchURL() + "/api/yt", fetcher);
 
+  const [cookiesAccepted, setCookiesAccepted] = useState()
   let videoHTMLData = [];
+
+  useEffect(() => {
+    if (window) {
+      setCookiesAccepted(checkIfFunctionalityCookiesAreAccepted())
+    }
+  }, [])
 
   if (error) {
     return <>
@@ -53,22 +63,33 @@ export default function DashboardPage() {
       </Header>
       <div className="container p-4 px-8 bg-slate-800 ">
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-          {!data || !data.xmlArray.length > 0 ?
-            <div className="h-auto max-w-full rounded-lg mt-4 mb-4">
-              <p className="mb-4 text-2xl text-white text-center pt-5 text-muted ">
-                Noch keine YouTube-Channels Hinzugefügt 😐
-              </p>
-              <div className="h-screen flex items-center justify-center">
-                <Add welcome={false} />
-              </div>
-            </div> :
-            <>
-              <div className="flex flex-wrap mt-8 mb-12 rounded justify-items-center items-center ">
-                {createVideosHTML(videoHTMLData, data.videoDisplayAmmount)}
-                <Donate />
-              </div>
-            </>
-          }
+          {cookiesAccepted ? <>
+            {!data || !data.xmlArray.length > 0 ?
+              <div className="h-auto max-w-full rounded-lg mt-10 pt-10 pb-10 mb-10">
+                <p className="mb-4 text-2xl text-white text-center pt-5 text-muted ">
+                  Noch keine YouTube-Channels Hinzugefügt 😐
+                </p>
+                <div className="h-screen flex items-center justify-center">
+                  <Add welcome={false} />
+                </div>
+              </div> :
+              <>
+                <div className="flex flex-wrap mt-8 mb-12 rounded justify-items-center items-center ">
+                  {createVideosHTML(videoHTMLData, data.videoDisplayAmmount)}
+                  <Donate />
+                </div>
+              </>
+            }
+          </> : <>
+            <div className="h-auto max-w-full rounded-lg mt-10 pt-4 pb-10 mb-10 text-center">
+              <h1 className="mb-4 text-xl text-white text-center pt-5 text-muted ">
+                Bitte akzeptieren Sie unsere Cookies, um SafeTube zu nutzen. 🍪
+              </h1>
+              <a className="text-blue-500 mt-4 hover:text-blue-500 transition duration-150 ease-in-out" href="#" id="open_preferences_center">
+                Cookie Einstellungen öffnen
+              </a>
+            </div>
+          </>}
         </div>
       </div>
     </>
